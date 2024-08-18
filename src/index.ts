184e3ddc6f7538c391b38d8d5a0fc85f7baa8529
Tyler Devereaux <tyler.devereaux@gmail.com>
2024-08-17 21:23:44 -0400

    converted util functions to static methods

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,121 @@
 enum LogLevel {
-    DEBUG = 0,
-    INFO = 1,
-    LOG = 2,
-    WARN = 4,
-    ERROR = 8,
+  DEBUG = 0,
+  INFO = 1,
+  LOG = 2,
+  WARN = 4,
+  ERROR = 8,
+}
+
+export class Logger {
+  label: string = "";
+
+  constructor(label: string) {
+    this.#init.call(this, label);
   }
-  
-  export function createLogger(name: string) {
+
+  #init(label?: string) {
+    if (label) {
+      this.label = label;
+    } else {
+      this.label = arguments.callee.arguments.callee.caller;
+    }
+  }
+
+  #output(type: LogLevel, args: any) {
+    try {
+      let isFirstArgString =
+        typeof args[0] === "string" || args[0] instanceof String;
+
+      if (type == LogLevel.DEBUG) {
+        if (isFirstArgString) {
+          args[0] = `[DEBUG] ${args[0]}`;
+        } else {
+          args.unshift("[DEBUG]");
+        }
+      }
+
+      if (isFirstArgString) {
+        args[0] = `[${this.label}] ${args[0]}`;
+      } else {
+        args.unshift(`[${this.label}]`);
+      }
+
+      let method: keyof Console;
+      switch (type) {
+        case LogLevel.DEBUG:
+          method = "debug";
+          break;
+        case LogLevel.ERROR:
+          method = "error";
+          break;
+        case LogLevel.INFO:
+          method = "info";
+          break;
+        case LogLevel.LOG:
+          method = "log";
+          break;
+        case LogLevel.WARN:
+          method = "warn";
+          break;
+        default:
+          throw new TypeError("Invalid log message type");
+      }
+      console[method].apply(this, args);
+    } catch (e: any) {
+      console.error("Unable to create log message", e);
+    }
+  }
+
+  debug(...args: any[]) {
+    this.#output(LogLevel.DEBUG, args);
+  }
+
+  error(...args: any[]) {
+    this.#output(LogLevel.ERROR, args);
+  }
+
+  group(label: string = "") {
+    console.group(`[${this.label}] ${label}`);
+  }
+
+  groupCollapsed(label: string = "") {
+    console.groupCollapsed(`[${this.label}] ${label}`);
+  }
+
+  groupEnd() {
+    console.groupEnd();
+  }
+
+  info(...args: any[]) {
+    this.#output(LogLevel.INFO, args);
+  }
+
+  log(...args: any[]) {
+    this.#output(LogLevel.LOG, args);
+  }
+
+  time(label: string = "") {
+    console.time(`[${this.label}] ${label}`);
+  }
+  timeEnd(label: string = "") {
+    console.timeEnd(`[${this.label}] ${label}`);
+  }
+  timeLog(label: string = "") {
+    console.timeLog(`[${this.label}] ${label}`);
+  }
+  warn(...args: any[]) {
+    this.#output(LogLevel.WARN, args);
+  }
+
+  public static Create(name: string) {
     const _logger = new Logger(name);
     return {
       _logger,
-      ...createLoggerShortcuts(_logger),
+      ...this.CreateShortcuts(_logger),
     };
   }
-  
-  export function createLoggerShortcuts(logger: Logger) {
+
+  public static CreateShortcuts(logger: Logger) {
     return {
       _: logger.log.bind(logger),
       _debug: logger.debug.bind(logger),
@@ -23,107 +124,6 @@ enum LogLevel {
       _warn: logger.warn.bind(logger),
     };
   }
-  
-  export class Logger {
-    label: string = "";
-  
-    constructor(label: string) {
-      this.#init.call(this, label);
-    }
-  
-    #init(label?: string) {
-      if (label) {
-        this.label = label;
-      } else {
-        this.label = arguments.callee.arguments.callee.caller;
-      }
-    }
-  
-    #output(type: LogLevel, args: any) {
-      try {
-        let isFirstArgString =
-          typeof args[0] === "string" || args[0] instanceof String;
-  
-        if (type == LogLevel.DEBUG) {
-          if (isFirstArgString) {
-            args[0] = `[DEBUG] ${args[0]}`;
-          } else {
-            args.unshift("[DEBUG]");
-          }
-        }
-  
-        if (isFirstArgString) {
-          args[0] = `[${this.label}] ${args[0]}`;
-        } else {
-          args.unshift(`[${this.label}]`);
-        }
-  
-        let method: keyof Console;
-        switch (type) {
-          case LogLevel.DEBUG:
-            method = "debug";
-            break;
-          case LogLevel.ERROR:
-            method = "error";
-            break;
-          case LogLevel.INFO:
-            method = "info";
-            break;
-          case LogLevel.LOG:
-            method = "log";
-            break;
-          case LogLevel.WARN:
-            method = "warn";
-            break;
-          default:
-            throw new TypeError("Invalid log message type");
-        }
-        console[method].apply(this, args);
-      } catch (e: any) {
-        console.error("Unable to create log message", e);
-      }
-    }
-  
-    debug(...args: any[]) {
-      this.#output(LogLevel.DEBUG, args);
-    }
-  
-    error(...args: any[]) {
-      this.#output(LogLevel.ERROR, args);
-    }
-  
-    group(label: string = "") {
-      console.group(`[${this.label}] ${label}`);
-    }
-  
-    groupCollapsed(label: string = "") {
-      console.groupCollapsed(`[${this.label}] ${label}`);
-    }
-  
-    groupEnd() {
-      console.groupEnd();
-    }
-  
-    info(...args: any[]) {
-      this.#output(LogLevel.INFO, args);
-    }
-  
-    log(...args: any[]) {
-      this.#output(LogLevel.LOG, args);
-    }
-  
-    time(label: string = "") {
-      console.time(`[${this.label}] ${label}`);
-    }
-    timeEnd(label: string = "") {
-      console.timeEnd(`[${this.label}] ${label}`);
-    }
-    timeLog(label: string = "") {
-      console.timeLog(`[${this.label}] ${label}`);
-    }
-    warn(...args: any[]) {
-      this.#output(LogLevel.WARN, args);
-    }
-  }
-  
-  export default Logger;
+}
+
+export default Logger;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,29 +1,29 @@
-import {Logger, createLogger, createLoggerShortcuts} from '../src/index'
+import Logger from "../src/index";
 
-describe('testing index file', () => {
-    test('Logger class can be instantiated', () => {
-        const logger = new Logger('test');
-        expect(logger).toBeInstanceOf(Logger);
-    });
+describe("testing index file", () => {
+  test("Logger class can be instantiated", () => {
+    const logger = new Logger("test");
+    expect(logger).toBeInstanceOf(Logger);
+  });
 
-    test('createLoggerShortcuts returns expected methods', () => {
-        const logger = new Logger('test');
-        const shortcuts = createLoggerShortcuts(logger);
+  test("CreateShortcuts returns expected methods", () => {
+    const logger = new Logger("test");
+    const shortcuts = Logger.CreateShortcuts(logger);
 
-        expect(shortcuts).toHaveProperty('_')
-        expect(shortcuts).toHaveProperty('_debug');
-        expect(shortcuts).toHaveProperty('_error');
-        expect(shortcuts).toHaveProperty('_info');
-        expect(shortcuts).toHaveProperty('_warn');
-    });
+    expect(shortcuts).toHaveProperty("_");
+    expect(shortcuts).toHaveProperty("_debug");
+    expect(shortcuts).toHaveProperty("_error");
+    expect(shortcuts).toHaveProperty("_info");
+    expect(shortcuts).toHaveProperty("_warn");
+  });
 
-    test('createLogger returns expected object', () => {
-        const shortcuts = createLogger('test');
-        expect(shortcuts).toHaveProperty('_')
-        expect(shortcuts).toHaveProperty('_debug');
-        expect(shortcuts).toHaveProperty('_error');
-        expect(shortcuts).toHaveProperty('_info');
-        expect(shortcuts).toHaveProperty('_logger');
-        expect(shortcuts).toHaveProperty('_warn');
-    })
+  test("Create returns expected object", () => {
+    const shortcuts = Logger.Create("test");
+    expect(shortcuts).toHaveProperty("_");
+    expect(shortcuts).toHaveProperty("_debug");
+    expect(shortcuts).toHaveProperty("_error");
+    expect(shortcuts).toHaveProperty("_info");
+    expect(shortcuts).toHaveProperty("_logger");
+    expect(shortcuts).toHaveProperty("_warn");
+  });
 });
