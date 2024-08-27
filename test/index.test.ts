3c74bfea8512f022c0938329a8d29c61a2bce7ad
dever23b <47509907+dever23b@users.noreply.github.com>
2024-08-26 21:15:51 -0400

    Add support for label function (#7)
    * Add typing
    
    * Bump version
    
    * add support for label function
    
    * update tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,29 +1,52 @@
 import Logger from "../src/index";
 
-describe("testing index file", () => {
-  test("Logger class can be instantiated", () => {
+describe("Logger class", () => {
+  it("can be instantiated", () => {
     const logger = new Logger("test");
     expect(logger).toBeInstanceOf(Logger);
   });
 
-  test("CreateShortcuts returns expected methods", () => {
+  it("logs properly with label function", () => {
+    const spy = jest.spyOn(console, "log");
     const logger = new Logger("test");
-    const shortcuts = Logger.CreateShortcuts(logger);
+    let i: number = 0;
 
-    expect(shortcuts).toHaveProperty("_");
-    expect(shortcuts).toHaveProperty("_debug");
-    expect(shortcuts).toHaveProperty("_error");
-    expect(shortcuts).toHaveProperty("_info");
-    expect(shortcuts).toHaveProperty("_warn");
+    logger.label = () => {
+      return `msg: ${++i}`;
+    };
+
+    logger.log("foo");
+    logger.log("bar");
+    logger.log("baz");
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenNthCalledWith(1, "[msg: 1] foo");
+    expect(spy).toHaveBeenNthCalledWith(2, "[msg: 2] bar");
+    expect(spy).toHaveBeenNthCalledWith(3, "[msg: 3] baz");
+    expect(spy).toHaveBeenCalledTimes(3);
   });
 
-  test("Create returns expected object", () => {
-    const shortcuts = Logger.Create("test");
-    expect(shortcuts).toHaveProperty("_");
-    expect(shortcuts).toHaveProperty("_debug");
-    expect(shortcuts).toHaveProperty("_error");
-    expect(shortcuts).toHaveProperty("_info");
-    expect(shortcuts).toHaveProperty("_logger");
-    expect(shortcuts).toHaveProperty("_warn");
+  describe("static method CreateShortcuts", () => {
+    it("returns expected methods", () => {
+      const logger = new Logger("test");
+      const shortcuts = Logger.CreateShortcuts(logger);
+
+      expect(shortcuts).toHaveProperty("_");
+      expect(shortcuts).toHaveProperty("_debug");
+      expect(shortcuts).toHaveProperty("_error");
+      expect(shortcuts).toHaveProperty("_info");
+      expect(shortcuts).toHaveProperty("_warn");
+    });
+  });
+
+  describe("static method Create", () => {
+    it("returns expected object", () => {
+      const shortcuts = Logger.Create("test");
+      expect(shortcuts).toHaveProperty("_");
+      expect(shortcuts).toHaveProperty("_debug");
+      expect(shortcuts).toHaveProperty("_error");
+      expect(shortcuts).toHaveProperty("_info");
+      expect(shortcuts).toHaveProperty("_logger");
+      expect(shortcuts).toHaveProperty("_warn");
+    });
   });
 });
